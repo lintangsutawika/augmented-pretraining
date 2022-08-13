3e54ed03d90f5efb6649232c578bb6def821ebeb
Lintang Sutawika <lintang@datasaur.ai>
2022-08-13 17:37:34 +0700

    can handle multiprocessing

--- build_dataset.py
+++ build_dataset.py
@@ -13,34 +13,34 @@ from tqdm import tqdm
 from urllib.parse import unquote
 
 parser = argparse.ArgumentParser()
-parser.add_argument("--index_path", default=None, type=str)
-parser.add_argument("--dump_path", default=None, type=str)
-parser.add_argument("--table_path", default=None, type=str)
-parser.add_argument("--redirect_table_path", default=None, type=str)
-parser.add_argument("--index_table_path", default=None, type=str)
+parser.add_argument("--wiki_dump_path", default=None, type=str)
+parser.add_argument("--page_list_path", default=None, type=str)
+parser.add_argument("--redirect_list_path", default=None, type=str)
+parser.add_argument("--data_frame_path", default=None, type=str)
 parser.add_argument("--samples", default=10, type=int)
+parser.add_argument("--num_proc", default=4, type=int)
 args = parser.parse_args()
 
 class WikiDump:
     """docstring for WikiDump"""
     def __init__(
         self,
-        index_path=None,
-        dump_path=None,
-        table_path=None,
-        redirect_table_path=None,
-        index_table_path=None,
+        wiki_dump_path=None,
+        page_list_path=None,
+        redirect_list_path=None,
+        data_frame_path=None,
+        num_proc=4,
         regex_url=None,
         regex_title=None,
         regex_word=None,
     ):
         super(WikiDump, self).__init__()
 
-        self.index_path = index_path
-        self.dump_path = dump_path
-        self.table_path = table_path
-        self.redirect_table_path = redirect_table_path
-        self.index_table_path = index_table_path
+        self.wiki_dump_path = wiki_dump_path
+        self.page_list_path = page_list_path
+        self.redirect_list_path = redirect_list_path
+        self.data_frame_path = data_frame_path
+        self.num_proc = num_proc
 
         if regex_url == None:
             self.regex_url = r"&lt;a(.*?)&lt;/a&gt;"
@@ -58,44 +58,52 @@ class WikiDump:
         else:
             self.regex_word = regex_word
 
-        if self.redirect_table_path is not None:
-            self.redirect_table = pd.read_csv(
-                self.redirect_table_path,
+        if self.redirect_list_path is not None:
+            self.redirect_df = pd.read_csv(
+                self.redirect_list_path,
                 sep="\t",
                 names=["id", "namespace", "title"]
             )
 
             import ast
             def reformat(x):
-                x = ast.literal_eval(
-                    "b"+x
-                ).decode('utf-8')
-                x = re.sub("_", " ", x)
+                try:
+                    if x[-1] != "\'":
+                        x = x+"\'"
+
+                    x = ast.literal_eval(
+                        "b"+x
+                    ).decode('utf-8')
+                    x = re.sub("_", " ", x)
+
+                except Exception as e:
+                    print("Got error: {}".format(e))
+                    print("On string: {}".format(x))
+
                 return x
 
-            self.redirect_table["title"] = self.redirect_table["title"].apply(reformat)
+            self.redirect_df["title"] = self.redirect_df["title"].apply(reformat)
 
-        if self.table_path is not None:
-            self.article_df = pd.read_pickle(self.table_path)
-            self.index_df = pd.read_pickle(self.index_table_path)
+        if (self.data_frame_path is not None) and (os.path.exists(self.data_frame_path)):
+            self.df = pd.read_pickle(self.data_frame_path)
         else:
-            with open(self.index_path, "r") as file:
-                self.article_df = pd.DataFrame(data={
+            with open(self.page_list_path, "r") as file:
+                self.df = pd.DataFrame(data={
                     "page": [re.sub("_", " ", page) for page in file.read().splitlines()[1:]],
                     })
 
-            self.article_df["id"] = np.nan
-            self.article_df["file_path"] = np.nan
-            self.article_df["in_degree"] = 0
-            self.article_df["linked"] = -1
+            self.df["id"] = np.nan
+            self.df["file_path"] = np.nan
+            self.df["in_degree"] = 0
+            self.df["linked"] = -1
 
-            dir_list = os.listdir(self.dump_path)
+            dir_list = os.listdir(self.wiki_dump_path)
             dir_list.sort()
 
             all_file_paths = []
             total_articles = 0
             for _dir in dir_list:
-                subdir = os.path.join(self.dump_path, _dir)
+                subdir = os.path.join(self.wiki_dump_path, _dir)
                 file_list = os.listdir(subdir)
                 file_list.sort()
 
@@ -123,7 +131,7 @@ class WikiDump:
 
                 article_list = list(article_file_path.keys())
 
-                _df = self.article_df[self.article_df['page'].isin(article_list)]
+                _df = self.df[self.df['page'].isin(article_list)]
                 _df['file_path'] = _df['page'].map(article_file_path)
                 _df['text'] = _df['page'].map(article_text)
                 _df['id'] = _df['page'].map(article_id)
@@ -133,31 +141,81 @@ class WikiDump:
 
                 _df['linked'] = _df['text'].apply(fn)
 
-                self.article_df.loc[_df.index,:]=_df[:]
+                self.df.loc[_df.index,:]=_df[:]
 
-            self.index_df = self.article_df[["page", "id"]]
-            self.index_df = self.index_df[self.index_df["id"].notnull()]
-            self.index_df.to_pickle("index_table.pkl")
+            # self.index_df = self.df[["page", "id"]]
+            # self.index_df = self.index_df[self.index_df["id"].notnull()]
+            # self.index_df.to_pickle("index_table.pkl")
 
-            self.article_df = self.article_df[
-                (self.article_df['file_path'].notnull()) & (self.article_df['linked'] != -1)
+            self.df = self.df[
+                (self.df['file_path'].notnull())
+                 # & (self.df['linked'] != -1)
             ]
 
+            if type(self.data_frame_path) == str:
+                self._process_link_count(self.data_frame_path)
+
+
+    def _calc_in_degree(self, link_list, idx=None, save_path=None):
+        for _i, link in tqdm(
+            enumerate(link_list),
+            total=len(link_list),
+            desc=f"#{idx}",
+            position=idx
+        ):
+            
+            _idx = self.df[self.df['page'].isin(link)].index
+            self.df.loc[_idx, "in_degree"] += 1
+
+        if save_path != None:
+            self.df.to_pickle(save_path)
 
     def _process_link_count(
         self,
         save_path
     ):
+        from multiprocessing import Process, Manager
 
-        self.article_df["in_degree"] = 0
-        for link in tqdm(self.article_df['linked'].tolist()):
-            idx = self.article_df[self.article_df['page'].isin(link)].index
-            self.article_df.loc[idx, "in_degree"] += 1
+        def _split(list_object, num_split):
+            k, m = divmod(len(list_object), num_split)
+            return [list_object[i*k+min(i, m):(i+1)*k+min(i+1, m)] for i in range(num_split)]
 
-        self.article_df = self.article_df.sort_values('in_degree', ascending=False)
-        self.article_df['in_degree'][self.article_df['in_degree'] == 0] = 1
+        num_proc = self.num_proc
+        if num_proc > 1:
+            processes = []
+            partitions = []
+            # creating processes
+            for idx, link_list in enumerate(_split(self.df['linked'][self.df['linked'] != -1].tolist(), num_proc)):
 
-        self.article_df.to_pickle(save_path)
+                _save_path = save_path.split(".")[0]+"_{}.pkl".format(idx)
+                p = Process(
+                    target=self._calc_in_degree,
+                    args=(link_list, idx, _save_path,)
+                )
+                p.start()
+                processes.append(p)
+                partitions.append(_save_path)
+
+            # completing process
+            for p in processes:
+                p.join()
+
+            for part in partitions:
+                df = pd.read_pickle(part)
+                idx = df[df['in_degree'] != 0].index
+                self.df.loc[idx, "in_degree"] += df.loc[idx, "in_degree"]
+                os.remove(part)
+
+        else:
+            self.df["in_degree"] = 0
+            for link in tqdm(self.df['linked'][self.df['linked'] != -1].tolist()):
+                idx = self.df[self.df['page'].isin(link)].index
+                self.df.loc[idx, "in_degree"] += 1
+
+        self.df = self.df.sort_values('in_degree', ascending=False)
+        self.df['in_degree'][self.df['in_degree'] == 0] = 1
+
+        self.df.to_pickle(save_path)
 
 
     def _process_url_title_string(
@@ -208,7 +266,6 @@ class WikiDump:
             end_idx = m.end()
 
             url_title_string = sampled_text[start_idx:end_idx]
-
             title_string = self._process_url_title_string(url_title_string)
 
             if title_string != False:
@@ -242,6 +299,26 @@ class WikiDump:
         return -1
 
 
+    def check_redirect(
+        self,
+        page_name,
+    ):
+
+        try:
+            link_id = self.df[
+                self.df["page"] == page_name
+            ]['id'].values[0]
+            redirect_page = self.redirect_df[
+                self.redirect_df["id"] == int(link_id)
+            ]["title"].values[0]
+            # print("TRUE")
+            # print("FROM {}".format(link))
+            # print("TO {}".format(redirect_link))
+            return redirect_page
+        except:
+            return page_name
+
+
     def get_sample(
         self,
         connection="contigous"
@@ -249,9 +326,12 @@ class WikiDump:
 
         assert connection in ["contigous", "random", "linked"]
 
-        sampled_article = self.article_df.sample(1)
+        sampled_article = self.df[self.df['linked'] != -1].sample(1)
         sampled_page = sampled_article["page"].values[0]
-        sampled_id = sampled_article["id"].values[0]
+        
+        # sampled_page = self.check_redirect(sampled_page)
+        # print(sampled_page)
+        # sampled_article = self.df[self.df["page"] == sampled_page]
         sampled_file_path = sampled_article["file_path"].values[0]
         sampled_links = sampled_article["linked"].values[0]
         sampled_links = [re.sub("_", " ", link) for link in sampled_links]
@@ -281,21 +361,6 @@ class WikiDump:
                 segment_b = sampled_text
         else:
 
-            for idx, link in enumerate(sampled_links):
-                try:
-                    link_id = self.index_df[
-                        self.index_df["page"] == link
-                    ]['id'].values[0]
-                    redirect_link = self.redirect_table[
-                        self.redirect_table["id"] == int(link_id)
-                    ]["title"].values[0]
-                    print("TRUE")
-                    print("FROM {}".format(link))
-                    print("TO {}".format(redirect_link))
-                    sampled_links[idx] = redirect_link
-                except:
-                    pass
-
             segment_a = sampled_text
             if connection == "random":
                 verbalizers = [
@@ -305,10 +370,12 @@ class WikiDump:
                 ]
 
                 not_sample_list = [sampled_page] + sampled_links
-                random_article = self.article_df[
-                    ~self.article_df["page"].isin(not_sample_list)
+                random_article = self.df[
+                    (self.df['linked'] != -1) & (~self.df["page"].isin(not_sample_list))
                     ].sample(1)
                 random_page = random_article['page'].values[0]
+                # random_page = self.check_redirect(random_page)
+                # random_article = self.df[self.df["page"] == random_page]
                 random_file_path = random_article['file_path'].values[0]
 
                 segment_b = self.get_article_text(
@@ -322,12 +389,17 @@ class WikiDump:
                     "{} is connected to {}"
                 ]
 
+                for idx, link in enumerate(sampled_links):
+                    redirect_link = self.check_redirect(link)
+                    if redirect_link != link:
+                        sampled_links[idx] = redirect_link
+
                 # Sampling which linked article to use
                 # is proportional to the inverse of the number of in-degrees an article has
                 # this is so that articles with high in-degrees are not over-sampled
-                linked_article = self.article_df[
-                    self.article_df["page"].isin(sampled_links)
-                    ].sample(1,weights=1/self.article_df["in_degree"])
+                linked_article = self.df[
+                    self.df["page"].isin(sampled_links)
+                    ].sample(1,weights=1/self.df["in_degree"])
 
                 linked_page = linked_article['page'].values[0]
                 linked_file_path = linked_article['file_path'].values[0]
@@ -344,15 +416,12 @@ if __name__ == '__main__':
 
     num_samples = args.samples
     wiki = WikiDump(
-        index_path=args.index_path,
-        dump_path=args.dump_path,
-        table_path=args.table_path,
-        redirect_table_path=args.redirect_table_path,
-        index_table_path=args.index_table_path
-        )
-
-
-
+        wiki_dump_path=args.wiki_dump_path,
+        page_list_path=args.page_list_path,
+        redirect_list_path=args.redirect_list_path,
+        data_frame_path=args.data_frame_path,
+        num_proc=args.num_proc
+    )
 
     # 1. Sample a text
     # 2. Parse through the URLs in the text
