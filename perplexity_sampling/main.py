4be737422cbdc9d782978f1b14f5f0c2a57c170e
lintangsutawika <lintang@sutawika.com>
2022-12-15 17:28:36 +0000

    changes

--- perplexity_sampling/main.py
+++ perplexity_sampling/main.py
@@ -15,34 +15,35 @@ from jax_smi import initialise_tracking
 initialise_tracking()
 
 import tensorflow as tf
-
-from jax.experimental.sparse import bcoo_reduce_sum
-
 tf.config.experimental.set_visible_devices([], "GPU")
-# tf.config.experimental.set_visible_devices([], "CPU")
 
-# def PP(w):
+def PP(w):
+    sbs = StupidBackoffSmoothing(matrix=matrix, k=args.k, N=args.N)
+    log_score = sbs.score(w, k=5)
+    return 10**(-log_score)
 
-#     def kneser_ney()
+def gaussian_sampling(alpha, beta, W, X):
 
-#     return 10**()
-
-# def gaussian_sampling(W):
-
-#     exponent = (-1/beta) * ((PP(W) - X)/X)**2
-#     return alpha * e ** exponent
+    exponent = (-1/beta) * jnp.power(((PP(W) - X)/X), 2)
+    return alpha * jnp.exp(exponent)
 
 os.environ["XLA_PYTHON_CLIENT_PREALLOCATE"] = "false"
 os.environ["XLA_PYTHON_CLIENT_MEM_FRACTION"] = '0.95'
 os.environ["xla_force_host_platform_device_count"] = '64'
 
 if __name__ == '__main__':
+
+    import sentencepiece as spm
+
     parser = argparse.ArgumentParser()
     parser.add_argument("--task", type=str)
     parser.add_argument("--batch_size", default=32, type=int)
-    parser.add_argument("--seq_length", default=512, type=int)
+    parser.add_argument("--seq_length", default=4096, type=int)
     parser.add_argument("--save_interval", default=500, type=int)
     parser.add_argument("--checkpoint_prefix", default="checkpoint_", type=str)
+    parser.add_argument("--vocab_path", type=str)
+    parser.add_argument("--matrix_path", default=None, type=str)
+    parser.add_argument("--ngram", default=5, type=int)
     args = parser.parse_args()
 
     seqio.add_global_cache_dirs(["/fsx/lintangsutawika/data/"])
@@ -60,31 +61,41 @@ if __name__ == '__main__':
     dataset = seqio.utils.trim_and_pad_dataset(dataset, sequence_length)
     dataset = dataset.batch(args.batch_size, drop_remainder=True)
 
-    k = 5
-    vocab_size = 1_000_000
-    matrix = util.init_matrix()
+    tokenizer = spm.SentencePieceProcessor(model_file=args.vocab_path)
 
-    increment_fn = jax.vmap(util.increment_at_coordinate, in_axes=[None, 0])
-    ngram_fn = jax.vmap(util.ngrams, [0,None])
+    k = args.ngram
+    vocab_size = tokenizer.vocab_size()
+    
     pad_fn = jax.vmap(util.pad, [0, None])
 
-    for idx, ex in tqdm(enumerate(dataset.as_numpy_iterator())):
+    if args.build_ngram_matrix:
+        
+        matrix = util.init_matrix()
+        increment_fn = jax.vmap(util.increment_at_coordinate, in_axes=[None, 0])
+        ngram_fn = jax.vmap(util.ngrams, [0,None])
 
-        all_x = []
-        for _k in [1,2,3,4,5]:
-            x = ngram_fn(ex['text'], _k)
-            x = jnp.reshape(x, (-1,_k))
-            x = pad_fn(x, k)
+        total_tokens = 0
 
-            all_x.append(x)
-        all_x = jnp.concatenate(all_x, axis=0)
+        for idx, ex in tqdm(enumerate(dataset.as_numpy_iterator())):
 
-        _matrix = increment_fn(util.init_matrix(), all_x)
-        matrix = matrix + _matrix.sum(0)
+            total_tokens += ex['text'][jnp.where(ex['text'] != 0)].shape[0]
+            
+            all_x = []
+            for _k in range(1, k+1):
+                x = ngram_fn(ex['text'], _k)
+                x = jnp.reshape(x, (-1,_k))
+                x = pad_fn(x, k)
 
-        jax.clear_backends()
+                all_x.append(x)
 
-        if (idx > 0) and (idx%args.save_interval == 0):
-            jnp.save("checkpoint_{}.npy".format(idx), matrix)
+            all_x = jnp.concatenate(all_x, axis=0)
+            all_x = all_x[jnp.where(all_x.sum(1) != 0)]
 
-    jnp.save("checkpoint_{}_finished.npy".format(idx), matrix)
+            matrix = matrix + increment_fn(matrix, all_x, k).sum(0)
+
+            # jax.clear_backends()
+
+            if (idx > 0) and (idx%args.save_interval == 0):
+                jnp.save("checkpoint_{}.npy".format(idx), matrix)
+
+        jnp.save("checkpoint_{}_finished.npy".format(idx), matrix)
